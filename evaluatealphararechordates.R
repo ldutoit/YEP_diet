439f8d5f5423bb70e83da213da728ccb708e47fc
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2020-05-11 15:30:27 +1200

    Update evaluatealphararechordates.R

--- evaluatealphararechordates.R
+++ evaluatealphararechordates.R
@@ -21,21 +21,21 @@
 	}
 
 
-	defmean_no<-function(x){return (mean(as.numeric(as.character(x)),na.rm=T))}
-	averagedata<-apply(newdata,2,defmean_no)
 
-	stdev_no_na<-function(x){return (sd(as.numeric(as.character(x)),na.rm=T))}
-	stderr<-apply(newdata,2,stdev_no_na)/sqrt(dim(newdata)[1])
-#Figure SX: Alpha rarefaction curves for ray-finned fishes. 175 samples with more than 3000 ray-finned fishes reads were randomly sampled for different number of sequences. The y-axis represents the average percentage of the total number of independent type of sequences (i.e. QIIME 2 features) within each sample detected at each depth. The error bars represent the .
 
-	pdf("sampled_alpha_chordates.pdf")
-	plot(x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)],pch=19,cex.axis=1.5,ylim=c(0,1.1),type="b",xlab="sampled depth",ylab = "% of total features detected",cex.lab=1.5)
-	arrows(x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)]-1.974*stderr[seq(1,100,by=10)],x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)]+1.974*stderr[seq(1,100,by=10)],length=0.05,angle=90,code=3)
-	dev.off()
+defmean_no<-function(x){return (mean(as.numeric(as.character(x)),na.rm=T))}
+averagedata<-apply(newdata,2,defmean_no)
+
+stdev_no_na<-function(x){return (sd(as.numeric(as.character(x)),na.rm=T))}
+stderr<-apply(newdata,2,stdev_no_na)/(sqrt(dim(newdata)[1]-34)) #(ignoring missing data)
+
+pdf("sampled_alpha_chordates.pdf")
+plot(x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)],pch=19,cex.axis=1.5,ylim=c(0,1.1),type="b",xlab="sampled depth",ylab = "% of total features detected",cex.lab=1.5)
+arrows(x[seq(1,100,by=10)],apply(newdata[seq(1,100,by=10)],2,quantile,na.rm=T,0.025),x[seq(1,100,by=10)],apply(newdata[seq(1,100,by=10)],2,quantile,na.rm=T,0.975),length=0.05,angle=90,code=3)
+dev.off()
 # for markdown file
-	png("/home/ludovic.dutoit/repos/scripts/YEP_diet/output/sampled_alpha_chordates.png")
-	plot(x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)],pch=19,cex.axis=1.5,ylim=c(0,1.1),type="b",xlab="sampled depth",ylab = "% of total features detected",cex.lab=1.5)
-	arrows(x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)]-1.974*stderr[seq(1,100,by=10)],x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)]+1.974*stderr[seq(1,100,by=10)],length=0.05,angle=90,code=3)
-	dev.off()
-
+png("/home/ludovic.dutoit/repos/scripts/YEP_diet/output/sampled_alpha_chordates.png")
+plot(x[seq(1,100,by=10)],averagedata[seq(1,100,by=10)],pch=19,cex.axis=1.5,ylim=c(0,1.1),type="b",xlab="sampled depth",ylab = "% of total features detected",cex.lab=1.5)
+arrows(x[seq(1,100,by=10)],apply(newdata[seq(1,100,by=10)],2,quantile,na.rm=T,0.025),x[seq(1,100,by=10)],apply(newdata[seq(1,100,by=10)],2,quantile,na.rm=T,0.975),length=0.05,angle=90,code=3)
+dev.off()
 
